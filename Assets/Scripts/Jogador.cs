2487d4f8eaf364f6e120f295b9a9206fee47eaac
Paulo Salvatore <salvatoregames@gmail.com>
2018-09-11 21:03:56 -0300

    Initial commit

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour {
+
+	public GameObject projetilPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Atirar();
+		}
+	}
+
+	private void Atirar()
+	{
+		GameObject projetil = Instantiate(projetilPrefab);
+		projetil.transform.position = Camera.main.transform.position;
+		projetil.transform.forward = Camera.main.transform.forward;
+	}
+}
---
+++ Assets/Scripts/Zumbi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zumbi : MonoBehaviour {
+
+	private Animator animator;
+	public Rigidbody rb;
+
+	public float delayAndar = 1f;
+	public float velocidade = 0.35f;
+
+	private bool andando = false;
+
+	private GameObject jogador;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+		jogador = GameObject.Find("Jogador");
+		transform.LookAt(jogador.transform);
+
+		Invoke("Andar", delayAndar);
+	}
+
+	void Andar()
+	{
+		andando = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (andando)
+		{
+			rb.velocity = transform.forward * velocidade;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Projétil"))
+		{
+			Destroy(other.gameObject);
+			Matar();
+		}
+	}
+
+	private void Matar()
+	{
+		andando = false;
+		animator.SetTrigger("Die");
+	}
+}
